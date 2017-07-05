8c77383e819d2470a5fec33fe16bd476bd149ad8
Garry Hill <garry@magnetised.net>
2017-07-05 16:53:29 +0100

    Prepare for release to Hex

--- mix.exs
+++ mix.exs
@@ -1,9 +1,11 @@
 defmodule Probe.Mixfile do
   use Mix.Project
 
+  @version "1.0.0"
+
   def project do
     [app: :probe,
-     version: "1.0.0",
+     version: @version,
      build_path: "_build",
      config_path: "config/config.exs",
      deps_path: "deps",
@@ -12,8 +14,11 @@ defmodule Probe.Mixfile do
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps(),
+     source_url: "https://github.com/strobe-audio/probe",
+     description: description(),
+     package: package(),
      name: "Probe",
-     docs: [main: "Probe"],
+     docs: [source_ref: "v#{@version}, "main: "Probe"],
     ]
   end
 
@@ -23,5 +28,18 @@ defmodule Probe.Mixfile do
 
   defp deps do
     [{:ex_doc, "~> 0.14", only: :dev, runtime: false}]
+  end
+
+  defp description do
+    """
+    A super-powered version of `IO.inspect` for better print debugging.
+    """
+  end
+
+  defp package do
+    [ maintainers: ["Garry Hill"],
+      licenses: ["MIT"],
+      links: %{"Github" => "https://github.com/strobe-audio/probe"}
+    ]
   end
 end
